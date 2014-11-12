4c0fb8f25f06e3002882cfae63aca595732f35fe
winterbe <winterbe@gmail.com>
2014-11-12 10:16:19 +0100

    copy javadoc files to _site

--- src/main/java/com/winterbe/javadoc/FileWalker.java
+++ src/main/java/com/winterbe/javadoc/FileWalker.java
@@ -1,11 +1,14 @@
 package com.winterbe.javadoc;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -22,14 +25,29 @@ public class FileWalker {
 
         List<String> paths = getPaths(basePath);
 
+        File site = new File("_site");
+        if (!site.exists()) {
+            Files.createDirectory(Paths.get("_site"));
+        }
+
+        System.out.println("clean site directory");
+        FileUtils.cleanDirectory(site);
+
+        System.out.println("copying javadoc files");
+        FileUtils.copyDirectory(new File(basePath), site);
+
         FileParser parser = new FileParser();
 
         List<TypeInfo> typeInfos = new ArrayList<>();
 
+        System.out.println("parsing type infos");
+
         for (int i = 0; i < paths.size(); i++) {
             String path = paths.get(i);
-            File file = new File(basePath + "/" + path);
-            Optional<TypeInfo> optional = parser.parse(file, path);
+            String sourcePath = basePath + "/" + path;
+            File sourceFile = new File(sourcePath);
+
+            Optional<TypeInfo> optional = parser.parse(sourceFile, path);
             optional.ifPresent(typeInfos::add);
 
 //            if (i == 500) {
